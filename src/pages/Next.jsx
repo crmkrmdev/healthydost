e878d5247175483e0c172a59342e0c4ac290630e
Xonu-dev <crm.krmayu2@gmail.com>
2025-04-18 14:49:06 +0530

    Add progress bar functionality to Next component

--- src/pages/Next.jsx
+++ src/pages/Next.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Next = () => {
+  const [progress, setProgress] = useState(0);
+
+  // continuously increase the progress bar
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 100); // Increase the speed of the progress bar
+  }, []);
+
   return (
     <div className="main">
       <div className="" style={{ width: "150px" }}>
@@ -17,7 +32,19 @@ const Next = () => {
           </div>
           <div className="d-flex flex-column justify-content-center align-items-center gap-2">
             <h4 className="">You are just one step away</h4>
-            <button className="btn btn-primary">Next</button>
+            {/* progress line*/}
+            <div
+              className="progress"
+              style={{ width: "300px", backgroundColor: "#e0e0e0" }}
+            >
+              <div
+                className="progress-bar bg-success"
+                role="progressbar"
+                style={{ width: `${progress}%` }}
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
           </div>
         </div>
       </div>
