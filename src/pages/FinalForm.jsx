587232552be7f50ee7dc6825a46b75a80edce628
Sarabjeet <crm.krmayu3@gmail.com>
2025-04-18 18:00:11 +0530

    `Added form fields for gender, weight, height, email, and phone to FinalForm component`

--- src/pages/FinalForm.jsx
+++ src/pages/FinalForm.jsx
@@ -4,7 +4,23 @@ const FinalForm = () => {
   const [formData, setFormData] = useState({
     name: "",
     age: "",
+    gender: "",
+    weight: "",
+    height: "",
+    email: "",
+    phone: "",
   });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center my-5">
       <div
@@ -12,10 +28,13 @@ const FinalForm = () => {
         style={{ width: "600px", minHeight: "100px" }}
       >
         <h3 className="mb-4 text-center">User Details</h3>
-        <form className="FinalForm">
+        <form className="FinalForm" onSubmit={handleSubmit}>
           <div className="mb-2">
             <label className="form-label text-white">Name</label>
             <input
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
               required
               type="text"
               className="form-control bg-transparent text-white border-light"
@@ -25,6 +44,9 @@ const FinalForm = () => {
           <div className="mb-2">
             <label className="form-label text-white">Age</label>
             <input
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
               required
               minvalue={0}
               type="number"
@@ -35,6 +57,9 @@ const FinalForm = () => {
           <div className="mb-2">
             <label className="form-label text-white">Gender</label>
             <select
+              name="gender"
+              value={formData.gender}
+              onChange={handleChange}
               className="form-select bg-transparent text-white border-light"
               required
             >
@@ -47,6 +72,9 @@ const FinalForm = () => {
           <div className="mb-2">
             <label className="form-label text-white">Weight (approx)</label>
             <input
+              name="weight"
+              onChange={handleChange}
+              value={formData.weight}
               required
               type="number"
               className="form-control bg-transparent text-white border-light"
@@ -56,6 +84,9 @@ const FinalForm = () => {
           <div className="mb-2">
             <label className="form-label text-white">Email</label>
             <input
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
               required
               type="email"
               className="form-control bg-transparent text-white border-light"
@@ -65,6 +96,9 @@ const FinalForm = () => {
           <div className="mb-2">
             <label className="form-label text-white">Phone</label>
             <input
+              name="phone"
+              onChange={handleChange}
+              value={formData.phone}
               required
               type="tel"
               className="form-control bg-transparent text-white border-light"
